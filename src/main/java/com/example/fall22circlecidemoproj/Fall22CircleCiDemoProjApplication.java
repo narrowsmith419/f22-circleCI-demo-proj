e3cb1723c50c0588587055beaab1c5e26a1290ba
narrowsmith419 <arrowsmith.nathan@gmail.com>
2022-11-30 15:55:03 -0800

    added initial files

---
+++ src/main/java/com/example/fall22circlecidemoproj/Fall22CircleCiDemoProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.fall22circlecidemoproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fall22CircleCiDemoProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fall22CircleCiDemoProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fall22circlecidemoproj/Fall22CircleCiDemoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fall22circlecidemoproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fall22CircleCiDemoProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
